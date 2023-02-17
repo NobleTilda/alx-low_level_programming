748b1a05d31c600179783eb0489ca3d6a0831834
Rachael Mamah <Rachael Mamah>
2023-02-16 18:26:22 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * A program that prints using puts function
+ * Return: 0 (Passed)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
