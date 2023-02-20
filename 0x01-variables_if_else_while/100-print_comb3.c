e77bc0479973c4b4de4507f837215fb44ac4751b
Rachael Mamah <Rachael Mamah>
2023-02-20 15:39:56 -0800

    Print Comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int h;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (h = 1 ; h < 10 ; h++)
+		{
+			if (i < h && i != h)
+			{
+				putchar(i + '0');
+				putchar(h + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
